120e8cf1d11374dca2a2c9d57c5ef51a718d48ef
Muhammad Ibnu Afif <ibnuafif015@gmail.com>
2023-12-29 23:10:55 +0700

    image

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="my-12 bg-[#FFFBF5]">
+      {/* Start Text */}
       <div className="flex flex-row md:justify-between mx-7 md:mx-32 pt-5 text-center md:items-center">
         <span className="text-3xl font-nohemi font-bold">
           Hey There, <br /> Im Ibnu
@@ -11,10 +12,20 @@ export default function Home() {
           I design beautiful websites, <br /> and i love what i do
         </span>
       </div>
-      <div className="flex justify-center my-1 items-center">
-        <Image src="/user3.png" alt="logo" width={400} height={50} className="absolute mt-0 z-50" />
-        <Image src="/bg2.png" alt="logo" width={400} height={500} className="z-10 " />
+
+      {/* End Text */}
+
+      {/* Start Image */}
+      <div className="flex flow-row justify-center my-1 items-center">
+        <div>
+          <Image src="/user3.png" alt="logo" width={400} height={50} className="absolute mt-0 z-50" />
+          <Image src="/bg2.png" alt="logo" width={400} height={500} className="z-10 " />
+        </div>
+        <span className="flex content-center">
+          <Image src="/Purwadhika.png" alt="logo" width={150} height={50} className="absolute mt-0 z-50" />
+        </span>
       </div>
+      {/* End Image */}
     </div>
   );
 }
