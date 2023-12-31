65c5673330223b69e397bb2d2be33c1e3be3468b
Muhammad Ibnu Afif <ibnuafif015@gmail.com>
2023-12-31 14:00:32 +0700

    title

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="my-12 bg-[#FFFBF5]">
+    <div className="my-12 bg-[#FFFBF5] text-center">
       {/* Start Text */}
-      <div className="flex flex-row md:justify-between mx-7 md:mx-32 pt-5 text-center md:items-center">
-        <span className="text-3xl font-nohemi font-bold">
-          Hey There, <br /> Im Ibnu
-        </span>
+      <div className="flex justify-center xl:justify-between mx-7 md:mx-32 pt-5 text-center md:items-center ">
+        <div className="text-3xl font-nohemi font-bold  flex flex-col w-full">
+          <span className="text-yellow-500">Hello,</span>
+          <span>I Am Ibnu.</span>
+          <span className="text-xl text-green-500 font-bold">Software Engineer</span>
+        </div>
+
         <span className=" hidden md:block font-codenext ">
           I design beautiful websites, <br /> and i love what i do
         </span>
@@ -20,9 +23,6 @@ export default function Home() {
           <Image src="/user3.png" alt="logo" width={400} height={50} className="absolute mt-0 z-50" />
           <Image src="/bg2.png" alt="logo" width={400} height={500} className="z-10 " />
         </div>
-        <span className="flex content-center">
-          <Image src="/Purwadhika.png" alt="logo" width={150} height={50} className="absolute mt-0 z-50" />
-        </span>
       </div>
       {/* End Image */}
     </div>
